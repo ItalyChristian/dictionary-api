484f1c9ae5fcffc16052ed80d0fe6bbddcc6e753
italy <italy.christian@hotmail.com>
2026-07-05 21:07:18 -0300

    feature: updates drizzle schema

--- src/infrastructure/database/drizzle-schema.ts
+++ src/infrastructure/database/drizzle-schema.ts
@@ -1,4 +1,4 @@
-import { pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core';
+import { integer, jsonb, pgTable, text, timestamp, uuid } from 'drizzle-orm/pg-core';
 
 export const users = pgTable('users', {
   id: uuid('id').primaryKey(),
@@ -11,4 +11,22 @@ export const userFavorites = pgTable('user_favorites', {
   userId: uuid('user_id').notNull(),
   wordId: text('word_id').notNull(),
   favoritedAt: timestamp('favorited_at').notNull()
+});
+
+export const words = pgTable('words', {
+  id: text('id').primaryKey(),
+  word: text('word').notNull().unique(),
+  meanings: jsonb('meanings').notNull(),
+  phonetics: jsonb('phonetics').notNull(),
+  favoriteCount: integer('favorite_count').notNull().default(0),
+  viewCount: integer('view_count').notNull().default(0),
+  createdAt: timestamp('created_at').notNull()
+});
+
+export const wordHistory = pgTable('word_history', {
+  id: uuid('id').primaryKey().defaultRandom(),
+  userId: uuid('user_id').notNull(),
+  wordId: text('word_id').notNull(),
+  word: text('word').notNull(),
+  viewedAt: timestamp('viewed_at').notNull()
 });

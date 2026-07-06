e6ab17cf05be1cf18477a9b43178f378e9c3195a
italy <italy.christian@hotmail.com>
2026-07-05 21:05:04 -0300

    feature: creates external api consumption file and updates cache

---
+++ src/infrastructure/api/types.ts
@@ -0,0 +1,19 @@
+export interface FreeDictionaryEntry {
+  word: string;
+  phonetic?: string;
+  phonetics?: {
+    text?: string;
+    audio?: string;
+  }[];
+  meanings?: {
+    partOfSpeech: string;
+    definitions: {
+      definition: string;
+      example?: string;
+      synonyms?: string[];
+      antonyms?: string[];
+    }[];
+    synonyms?: string[];
+    antonyms?: string[];
+  }[];
+}
